de6a3a11979843cb28cd4df161e04fe033f4257f
jochoa <1128269848>
2019-11-01 20:04:38 -0500

    Primer commit

---
+++ src/main/java/com/practicajo/backendjuano/BackendjuanoApplication.java
@@ -0,0 +1,13 @@
+package com.practicajo.backendjuano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendjuanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendjuanoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practicajo/backendjuano/BackendjuanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicajo.backendjuano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendjuanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
